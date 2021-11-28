16642a6713c2a5cb47c418803bdd3b6058a1a094
assafvan <vanunuassaf@gmail.com>
2021-11-28 03:21:37 +0200

    final commit

---
+++ app.js
@@ -0,0 +1,45 @@
+function addToCart() {
+  const quantity = document.querySelector(".qty-opt");
+  let numOfCartItems = document.querySelector(".cart-items-number");
+  if (quantity.value === "1") {
+    numOfCartItems.textContent = "1";
+  } else if (quantity.value === "2") {
+    numOfCartItems.textContent = "2";
+  }
+}
+function changeImageOneOnHover() {
+  const imageOne = document.querySelector(".big-img");
+  imageOne.style.backgroundImage =
+    "url(../Assets/Img/619CzvIvzsL._AC_SX450_.jpg)";
+  imageOne.style.backgroundSize = "contain";
+}
+function changeImageTwoOnHover() {
+  const imageTwo = document.querySelector(".big-img");
+  imageTwo.style.backgroundImage =
+    "url(../Assets/Img/81cpPCoVPvL._AC_SY450_.jpg)";
+  imageTwo.style.backgroundSize = "contain";
+}
+function changeImageThreeOnHover() {
+  const imageThree = document.querySelector(".big-img");
+  imageThree.style.backgroundImage =
+    "url(../Assets/Img/81eXVpVaxhL._AC_SY450_.jpg)";
+  imageThree.style.backgroundSize = "cover";
+}
+function changeImageFourOnHover() {
+  const imageFour = document.querySelector(".big-img");
+  imageFour.style.backgroundImage =
+    "url(../Assets/Img/519g5Z-t6dL._AC_SY450_.jpg)";
+  imageFour.style.backgroundSize = "contain";
+}
+function changeImageFiveOnHover() {
+  const imageFive = document.querySelector(".big-img");
+  imageFive.style.backgroundImage =
+    "url(../Assets/Img/51BiqzvXBBL._AC_SY450_.jpg)";
+  imageFive.style.backgroundSize = "contain";
+}
+function changeImageSixOnHover() {
+  const imageSix = document.querySelector(".big-img");
+  imageSix.style.backgroundImage =
+    "url(../Assets/Img/81WsfwaEJ1L._AC_SY450_.jpg)";
+  imageSix.style.backgroundSize = "contain";
+}
